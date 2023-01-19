b8e264328b938f817c34eb8a1a9caebc11d463e4
S <Shashankkirtania@gmail.com>
2023-01-19 23:09:18 +0530

    removed key

--- notouch/notouch/settings.py
+++ notouch/notouch/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!zt)ef$d3@ulz1jxqsvylq0d5kq9f*x%)!+d9^e9(60uw))4gb'
+SECRET_KEY = 'key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
